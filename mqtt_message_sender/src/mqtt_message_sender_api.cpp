6b69fbc0dad07ffaeb4f0de40c506264be8766d9
wenhao <wenhao.com>
2026-04-24 09:57:01 +0800

    fixSendMqttMessageFun

--- mqtt_message_sender/src/mqtt_message_sender_api.cpp
+++ mqtt_message_sender/src/mqtt_message_sender_api.cpp
@@ -1,3 +1,4 @@
+#include <thread>
 #include "../include/message_sender_magager.h" // 包含message_sender_magager.h头文件以使用MessageSenderManager类的定义
 #include "../api/mqtt_message_sender_api.h" // 包含mqtt_message_sender_api.h头文件以使用其中定义的结构体和函数声明
 
@@ -7,12 +8,24 @@ bool SendMqttMessage(const MqttMessageStruct& message)
         return false; // 发送器未准备好，无法发送消息
     }
 
-    if (MessageSenderManager::GetInstance().IsMessageSenderIdle()) {
-        // 拷贝发送的信息
-        MessageSenderManager::GetInstance().CopyMessage(message);
-        // 设置发送标志位，通知发送线程可以发送消息了
-        MessageSenderManager::GetInstance().SetMessageSendSwitch(MESSAGE_SEND_SWITCH_ON);
-        return true; // 或者可以添加等待机制，直到message_sender准备好
+    uint8_t retryCount = 0; // 定义重试次数
+    const uint8_t maxRetryCount = 3; // 定义最大重试次数
+
+    while (true)
+    {
+        if (MessageSenderManager::GetInstance().IsMessageSenderIdle()) {
+            // 拷贝发送的信息
+            MessageSenderManager::GetInstance().CopyMessage(message);
+            // 设置发送标志位，通知发送线程可以发送消息了
+            MessageSenderManager::GetInstance().SetMessageSendSwitch(MESSAGE_SEND_SWITCH_ON);
+            return true; // 或者可以添加等待机制，直到message_sender准备好
+        }
+        retryCount++;
+        if (retryCount >= maxRetryCount) {
+            return false; // 超过最大重试次数，放弃发送
+        }
+        // 可以添加适当的睡眠时间，避免CPU占用过高
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
     return false;
 }
