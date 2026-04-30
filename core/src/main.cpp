a50a7626ee552781bfa75b6a4b101cbaad9ec6a9
WenHao19961022 <1803019417@qq.com>
2026-04-30 14:41:00 +0800

    addCoutMutex

--- core/src/main.cpp
+++ core/src/main.cpp
@@ -1,25 +1,35 @@
 #include <iostream>
 #include <thread>
+#include <mutex>
 #include "../include/external_apis.h"
 #include "../include/core_manager.h"
 
+
+// 全局互斥锁
+std::mutex cout_mutex;
+
+
 // 模块函数声明
 void LaunchMqttMessageSender() {
+    std::lock_guard<std::mutex> lock(cout_mutex);
     std::cout << "MqttMessageSender running in thread " << std::this_thread::get_id() << std::endl;
     MqttMessageSenderMainLoop();
 }
 
 void LaunchStm32MessageReceiver() {
+    std::lock_guard<std::mutex> lock(cout_mutex);
     std::cout << "Stm32MessageReceiver running in thread " << std::this_thread::get_id() << std::endl;
     Stm32MessageReceverMainLoop();
 }
 
 void LaunchCvModel() {
+    std::lock_guard<std::mutex> lock(cout_mutex);
     std::cout << "CvModel running in thread " << std::this_thread::get_id() << std::endl;
     CvModelMainLoop();
 }
 
 void Core() {
+    std::lock_guard<std::mutex> lock(cout_mutex);
     std::cout << "Core running in thread " << std::this_thread::get_id() << std::endl;
     // 主线程具体逻辑
     CoreManager manager;
